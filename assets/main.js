05275ff3040b9ff1ec7aa24665c9b12801b4cd3c
jenny <jenny@MBP-de-Jenny.localdomain>
2022-03-15 16:46:07 +0100

    add comments

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,17 @@
 console.log("hello from assets/main.js");
 
-
 // document.getElementById('main-title').innerHTML = data[0].title;
 // document.getElementById('sub-title').innerHTML = data[0].name;
 // document.getElementById('main-picture').src = '/assets/images/' + data[0].avatar;
 
+// je target l'element avec l'id burger menu dans layouts/header.php et ecoute l'event click
 document.getElementById('burger-menu').addEventListener('click', (event) => {
     console.log('click', event);
+    // au click je cible l'element avec l'id nav-mobile-list et je le rend visible grace au toggle 'close' qui correspond a une class css
     document.getElementById('nav-mobile-list').classList.toggle('close');
 });
 
+// meme logique que pour le burger menu avec le dark mode
 document.getElementById('theme').addEventListener('click', (event) => {
     console.log('click', event);
     document.getElementById('main').classList.toggle('dark');
